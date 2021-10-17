9921616af641d0db9b35179eeda77891584b7460
coppermilk <nataliyakowtun67@gmail.com>
2021-10-17 20:12:13 +0300

    Create  Bumblebee-humidifier_v2.ino

---
+++ Bumblebee-humidifier_v2.ino
@@ -0,0 +1,79 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// https://github.com/rkoptev/ACS712-arduino
+#include "ACS712.h"
+
+/*
+  This example shows how to measure the power consumption
+  of devices in 230V electrical system
+  or any other system with alternative current
+*/
+
+// We have 30 amps version sensor connected to A0 pin of arduino
+// Replace with your version if necessary
+ACS712 sensor(ACS712_30A, A8);
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 8,
+          en = 9,
+          d4 = 4,
+          d5 = 5,
+          d6 = 6,
+          d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+        Serial.begin(9600);
+        pinMode(2, OUTPUT); // устанавливает режим работы - выход
+        
+        // calibrate() method calibrates zero point of sensor,
+        // It is not necessary, but may positively affect the accuracy
+        // Ensure that no current flows through the sensor at this moment
+        // If you are not sure that the current through the sensor will not leak during calibration - comment out this method
+        Serial.println("Calibrating... Ensure that no current flows through the sensor at this moment");
+        sensor.calibrate();
+        Serial.println("Done!");
+        // set up the LCD's number of columns and rows:
+        lcd.begin(16, 2);
+        // Print a message to the LCD.
+        //lcd.print("hello, world!");
+        //analogWrite(10,5);
+}
+
+void loop() {
+        // set the cursor to column 0, line 1
+        // (note: line 1 is the second row, since counting begins with 0):
+        lcd.setCursor(0, 0);
+        lcd.print("T: ");
+        lcd.print(millis() / 1000);
+
+        // We use 230V because it is the common standard in European countries
+        // Change to your local, if necessary
+        float U = 230;
+
+        // To measure current we need to know the frequency of current
+        // By default 50Hz is used, but you can specify desired frequency
+        // as first argument to getCurrentAC() method, if necessary
+        float I = sensor.getCurrentAC();
+
+        // To calculate the power we need voltage multiplied by current
+        float P = U * I;
+
+        Serial.println(String("I = ") + I + " A");
+        Serial.println(String("P = ") + P + " Watts");
+
+        lcd.setCursor(0, 1);
+        // print the number of seconds since reset:
+        lcd.print(String("P = ") + P + " Watts");
+        if (P < 10) {
+                digitalWrite(2, HIGH);
+                delay(10 * 1000);
+                digitalWrite(2, LOW);
+        }
+        
+        //lcd.clear();
+        delay(1000);
+
+}
